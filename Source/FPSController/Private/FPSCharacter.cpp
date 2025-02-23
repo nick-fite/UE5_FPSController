dc192509fa12e45ce264b50f9d7d5e936068ce4d
nick-fite <70393136+nick-fite@users.noreply.github.com>
2025-02-22 20:34:25 -0600

    init

---
+++ Source/FPSController/Private/FPSCharacter.cpp
@@ -0,0 +1,120 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+// Sets default values
+AFPSCharacter::AFPSCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("Camera Boom");
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->bUsePawnControlRotation = true;
+	CameraBoom->TargetArmLength = 0.f;
+
+	bUseControllerRotationYaw = false;
+
+	ViewCam = CreateDefaultSubobject<UCameraComponent>("View Cam");
+	ViewCam->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(1200.f);
+	
+	
+
+}
+
+// Called when the game starts or when spawned
+void AFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	if(APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if(UEnhancedInputLocalPlayerSubsystem* subsys = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			subsys->RemoveMappingContext(MappingContext);
+			subsys->AddMappingContext(MappingContext, 0);
+		}
+	}
+}
+
+// Called every frame
+void AFPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if(EnhancedInputComponent)
+	{
+		EnhancedInputComponent->BindAction(LookInputAction, ETriggerEvent::Triggered, this, &AFPSCharacter::HandleLookInput);
+		EnhancedInputComponent->BindAction(MoveInputAction, ETriggerEvent::Triggered, this, &AFPSCharacter::HandleMoveInput);
+		EnhancedInputComponent->BindAction(SlideInputAction, ETriggerEvent::Triggered, this, &AFPSCharacter::HandleSlideInput);
+		EnhancedInputComponent->BindAction(JumpInputAction, ETriggerEvent::Triggered, this, &AFPSCharacter::Jump);
+	}
+}
+
+FVector AFPSCharacter::GetLookRightDirection() const
+{
+	return ViewCam->GetRightVector();
+}
+
+FVector AFPSCharacter::GetLookForwardDirection() const
+{
+	return ViewCam->GetForwardVector();
+}
+
+FVector AFPSCharacter::GetMoveForwardDirection() const
+{
+	return FVector::CrossProduct(GetLookRightDirection(), FVector::UpVector);
+}
+
+void AFPSCharacter::HandleLookInput(const FInputActionValue& InputActionValue)
+{
+	FVector2D InputVal = InputActionValue.Get<FVector2D>();
+	AddControllerYawInput(InputVal.X);
+	AddControllerPitchInput(-InputVal.Y);
+}
+
+void AFPSCharacter::HandleMoveInput(const FInputActionValue& InputActionValue)
+{
+	FVector2D InputVal = InputActionValue.Get<FVector2D>();
+	if(InputVal.Length() < 0)
+	{
+		return;
+	}
+	InputVal.Normalize();
+	AddMovementInput(GetLookRightDirection() * InputVal.X + GetMoveForwardDirection() * InputVal.Y);
+}
+
+void AFPSCharacter::HandleSlideInput(const FInputActionValue& InputActionValue)
+{
+	bool InputVal = InputActionValue.Get<bool>();
+	
+	bIsSliding = InputVal;
+	UE_LOG(LogTemp, Warning, TEXT("Setting slide %hhd"), InputVal);
+	if(bIsSliding)
+	{
+		GetCharacterMovement()->Crouch();
+		GetCharacterMovement()->MaxWalkSpeed = 30000;
+		GetCharacterMovement()->CrouchedHalfHeight = 5;
+	}
+	else
+	{
+		GetCharacterMovement()->UnCrouch();
+		GetCharacterMovement()->bWantsToCrouch = false;
+		GetCharacterMovement()->MaxWalkSpeed = 600;
+		GetCharacterMovement()->CrouchedHalfHeight = 40;
+	}
+}
---
+++ Source/FPSController/Private/FPSCharacter.h
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EnhancedInputComponent.h"
+#include "FPSCharacter.generated.h"
+
+class UCameraComponent;
+
+UCLASS()
+class AFPSCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFPSCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	//Inputs
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputMappingContext* MappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* LookInputAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* MoveInputAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* JumpInputAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* SlideInputAction;
+	
+	//Camera
+	UPROPERTY(VisibleDefaultsOnly, Category="View")
+	class UCameraComponent* ViewCam;
+
+	UPROPERTY(VisibleDefaultsOnly, Category="View")
+	class USpringArmComponent* CameraBoom;
+
+	FVector GetLookRightDirection() const;
+	FVector GetLookForwardDirection() const;
+	FVector GetMoveForwardDirection() const;
+
+	void HandleLookInput(const FInputActionValue& InputActionValue);
+	void HandleMoveInput(const FInputActionValue& InputActionValue);
+	void HandleSlideInput(const FInputActionValue& InputActionValue);
+
+	//movement;
+	UPROPERTY(VisibleDefaultsOnly, Category = "Movement")
+	bool bIsSliding;
+};
